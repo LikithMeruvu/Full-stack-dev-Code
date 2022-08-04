d102172ef9730042b9f3e689d171047bf9a06a11
LikithMeruvu <codebook369@gmail.com>
2022-08-04 20:11:09 +0530

    file change

---
+++ JavaScript/js exercises/trusyvalues.js
@@ -0,0 +1,14 @@
+
+const arr = [null ,undefined, 0,'',true,false ,1 , 2 ,NaN];
+
+console.log(counttrusy(arr));
+
+function counttrusy(arr) {
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]) count++;
+    }
+    return count;
+}
+
+// function to count trusy values using if statements
