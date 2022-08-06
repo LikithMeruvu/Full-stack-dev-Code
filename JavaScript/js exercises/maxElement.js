822df525579520314d3a0e2172beecbd186903a3
LikithMeruvu <codebook369@gmail.com>
2022-08-06 21:56:02 +0530

    js basics

--- JavaScript/js exercises/maxElement.js
+++ JavaScript/js exercises/maxElement.js
@@ -1,19 +1,19 @@
-
-let arr = [7,89,890,6868];
+let arr = [7, 89, 890, 6868];
 
 const max = getMax(arr);
 console.log(max);
 
 function getMax(arr) {
-    if(arr.length === 0) return undefined;
-    // let max = arr[0];
-    // for(let i of arr) 
-    //     if(i >max )
-    //         max =i;
-    // return max;
+  if (arr.length === 0) return undefined;
+  // let max = arr[0];
+  // for(let i of arr)
+  //     if(i >max )
+  //         max =i;
+  // return max;
 
-    //optimised code
-    let max = arr.reduce((a,c)=> (a>c)?a:c);
-    // by default if we dont keep any value for the accumulator then it takes the starting value of the array
-    return max;
+  //optimised code
+  let max = arr.reduce((a, c) => (a > c ? a : c));
+  // by default if we dont keep any value for the accumulator then it takes the starting value of the array
+  return max;
 }
+
--- JavaScript/js exercises/showproperties.js
+++ JavaScript/js exercises/showproperties.js
@@ -1,18 +1,17 @@
-
-const obj ={
-    title:'avengers',
-    money : 10000000000,
-    description:'good movie',
-    best_actor : 'RDJ',
-    best_role :'iron man'
+const obj = {
+  title: "avengers",
+  money: 10000000000,
+  description: "good movie",
+  best_actor: "RDJ",
+  best_role: "iron man",
 };
 
-function showproperties(obj){
-    for (const key in obj) {
-       if(typeof obj[key] === 'string') {
-            console.log(obj[key]);
-       }
+function showproperties(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === "string") {
+      console.log(obj[key]);
     }
+  }
 }
 
 console.log(showproperties(obj));
