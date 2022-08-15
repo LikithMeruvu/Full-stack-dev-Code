d52fca26d594ca7273c85aaa76ab6bd3b555d36c
LikithMeruvu <codebook369@gmail.com>
2022-08-15 18:08:02 +0530

    Jquery and ADV DOM

--- Adv DOM/index.js
+++ Adv DOM/index.js
@@ -7,13 +7,22 @@ function PlaySong(a) {
     songarr[i].pause();
   }
 }
+function PauseSong(a) {
+  a.pause();
+}
 
 let s1 = new Audio("Music/1.mp3");
 let c1 = 0;
 
 document.querySelector(".p1").addEventListener("click", function () {
   c1++;
-  if (c1 % 2 !== 0) return document.querySelector(".current").innerHTML = `Heat Waves Glass animals`,PlaySong(s1);
+  if (c1 % 2 !== 0)
+    return (
+      (document.querySelector(
+        ".current"
+      ).innerHTML = `Heat Waves Glass animals`),
+      PlaySong(s1)
+    );
   else return s1.pause();
 });
 
@@ -25,7 +34,13 @@ let c2 = 0;
 
 document.querySelector(".p2").addEventListener("click", function () {
   c2++;
-  if (c2 % 2 !== 0) return document.querySelector(".current").innerHTML = `Sweater wheather X after dark`,PlaySong(s2);
+  if (c2 % 2 !== 0)
+    return (
+      (document.querySelector(
+        ".current"
+      ).innerHTML = `Sweater wheather X after dark`),
+      PlaySong(s2)
+    );
   else return s2.pause();
 });
 
@@ -41,7 +56,13 @@ let c3 = 0;
 
 document.querySelector(".p3").addEventListener("click", function () {
   c3++;
-  if (c3 % 2 !== 0) return document.querySelector(".current").innerHTML = `Running up hill Totem Remix`,PlaySong(s3);
+  if (c3 % 2 !== 0)
+    return (
+      (document.querySelector(
+        ".current"
+      ).innerHTML = `Running up hill Totem Remix`),
+      PlaySong(s3)
+    );
   else return s3.pause();
 });
 
@@ -57,7 +78,11 @@ let c4 = 0;
 
 document.querySelector(".p4").addEventListener("click", function () {
   c4++;
-  if (c4 % 2 !== 0) return document.querySelector(".current").innerHTML = `Fraco Dead To me`,PlaySong(s4);
+  if (c4 % 2 !== 0)
+    return (
+      (document.querySelector(".current").innerHTML = `Fraco Dead To me`),
+      PlaySong(s4)
+    );
   else return s4.pause();
 });
 
@@ -73,7 +98,11 @@ let c5 = 0;
 
 document.querySelector(".p5").addEventListener("click", function () {
   c5++;
-  if (c5 % 2 !== 0) return document.querySelector(".current").innerHTML = `Ghostamane Fed up`,PlaySong(s5);
+  if (c5 % 2 !== 0)
+    return (
+      (document.querySelector(".current").innerHTML = `Ghostamane Fed up`),
+      PlaySong(s5)
+    );
   else return s5.pause();
 });
 
@@ -89,7 +118,11 @@ let c6 = 0;
 
 document.querySelector(".p6").addEventListener("click", function () {
   c6++;
-  if (c6 % 2 !== 0) return document.querySelector(".current").innerHTML = `Sia Unstoppable`,PlaySong(s6);
+  if (c6 % 2 !== 0)
+    return (
+      (document.querySelector(".current").innerHTML = `Sia Unstoppable`),
+      PlaySong(s6)
+    );
   else return s6.pause();
 });
 
@@ -105,7 +138,13 @@ let c7 = 0;
 
 document.querySelector(".p7").addEventListener("click", function () {
   c7++;
-  if (c7 % 2 !== 0) return document.querySelector(".current").innerHTML = `WeekEnd Blinding Lights`,PlaySong(s7);
+  if (c7 % 2 !== 0)
+    return (
+      (document.querySelector(
+        ".current"
+      ).innerHTML = `WeekEnd Blinding Lights`),
+      PlaySong(s7)
+    );
   else return s7.pause();
 });
 
@@ -121,7 +160,13 @@ let c8 = 0;
 
 document.querySelector(".p8").addEventListener("click", function () {
   c8++;
-  if (c8 % 2 !== 0) return document.querySelector(".current").innerHTML = `Sia And Zayn Dusk Till Dawn`,PlaySong(s8);
+  if (c8 % 2 !== 0)
+    return (
+      (document.querySelector(
+        ".current"
+      ).innerHTML = `Sia And Zayn Dusk Till Dawn`),
+      PlaySong(s8)
+    );
   else return s8.pause();
 });
 
@@ -137,7 +182,11 @@ let c9 = 0;
 
 document.querySelector(".p9").addEventListener("click", function () {
   c9++;
-  if (c9 % 2 !== 0) return document.querySelector(".current").innerHTML = `Murder in my mind`,PlaySong(s9);
+  if (c9 % 2 !== 0)
+    return (
+      (document.querySelector(".current").innerHTML = `Murder in my mind`),
+      PlaySong(s9)
+    );
   else return s9.pause();
 });
 
@@ -150,44 +199,91 @@ document.querySelector(".fp9").addEventListener("click", function () {
 
 let songarr = [s1, s2, s3, s4, s5, s6, s7, s8, s9];
 
-s1.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s1.currentTime/s1.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s1.addEventListener("timeupdate", () => {
+  let progress = parseInt((s1.currentTime / s1.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s2.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s2.currentTime/s2.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s2.addEventListener("timeupdate", () => {
+  let progress = parseInt((s2.currentTime / s2.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s3.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s3.currentTime/s3.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s3.addEventListener("timeupdate", () => {
+  let progress = parseInt((s3.currentTime / s3.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s4.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s4.currentTime/s4.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s4.addEventListener("timeupdate", () => {
+  let progress = parseInt((s4.currentTime / s4.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s5.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s5.currentTime/s5.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s5.addEventListener("timeupdate", () => {
+  let progress = parseInt((s5.currentTime / s5.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s6.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s6.currentTime/s6.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s6.addEventListener("timeupdate", () => {
+  let progress = parseInt((s6.currentTime / s6.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s7.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s7.currentTime/s7.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s7.addEventListener("timeupdate", () => {
+  let progress = parseInt((s7.currentTime / s7.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s8.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s8.currentTime/s8.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s8.addEventListener("timeupdate", () => {
+  let progress = parseInt((s8.currentTime / s8.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
-s9.addEventListener("timeupdate",()=>{
-    let progress = parseInt((s9.currentTime/s9.duration)*100);
-    document.getElementById("ProgressBar").value = progress;
+s9.addEventListener("timeupdate", () => {
+  let progress = parseInt((s9.currentTime / s9.duration) * 100);
+  document.getElementById("ProgressBar").value = progress;
 });
 
-function isPlaying(s){
-  if(s.play() === true) return true;
-  else return false;
-}
+
+let a = 0;
+document.addEventListener("keypress", function (e) {
+  switch (e.key) {
+    case "1":
+      a++;
+      if (a % 2 !== 0) PlaySong(s1);
+      else PauseSong(s1);
+      break;
+    case "2":
+      a++;
+      if (a % 2 !== 0) PlaySong(s2);
+      else PauseSong(s2);
+      break;
+    case "3":
+      a++;
+      if (a % 2 !== 0) PlaySong(s3);
+      else PauseSong(s3);
+      break;
+    case "4":
+      a++;
+      if (a % 2 !== 0) PlaySong(s4);
+      else PauseSong(s4);
+      break;
+    case "5":
+      a++;
+      if (a % 2 !== 0) PlaySong(s5);
+      else PauseSong(s5);
+      break;
+    case "6":
+      a++;
+      if (a % 2 !== 0) PlaySong(s6);
+      else PauseSong(s6);
+      break;
+    case "7":
+      a++;
+      if (a % 2 !== 0) PlaySong(s7);
+      else PauseSong(s7);
+      break;
+    case "8":
+      a++;
+      if (a % 2 !== 0) PlaySong(s8);
+      else PauseSong(s8);
+      break;
+    case "9":
+      a++;
+      if (a % 2 !== 0) PlaySong(s9);
+      else PauseSong(s9);
+      break;
+  }
+});
