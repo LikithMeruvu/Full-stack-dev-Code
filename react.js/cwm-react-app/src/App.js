09c895aaae32aa7e420e13fec4635822fd01ea9d
LikithMeruvu <codebook369@gmail.com>
2022-09-19 17:16:55 +0530

    react

--- react.js/cwm-react-app/src/App.js
+++ react.js/cwm-react-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import TextToSpeech from './components/TextToSpeech'
 
 function App() {
 
   return (
     <>
-    
+        <TextToSpeech />
     </>
   );
 }
---
+++ react.js/cwm-react-app/src/components/TextToSpeech.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TextToSpeech extends Component {
+  render() {
+    return (
+      <>
+            
+      </>
+    )
+  }
+}
